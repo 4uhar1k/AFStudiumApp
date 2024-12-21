51fb3394b2ec026d1993617b2d29caf57d73ad11
4uhar1k <vovas0712@gmail.com>
2024-12-22 00:31:05 +0100

    fixed some bugs with editing

--- AFStudiumApp/Views/AddEventPage.xaml.cs
+++ AFStudiumApp/Views/AddEventPage.xaml.cs
@@ -10,6 +10,7 @@ public partial class AddEventPage : ContentPage
 {
 	private readonly AFStudiumAPIClientService _apiClient;
 	public ModulesViewModel thisContext { get; set; }
+	public Subject subject { get; set; }
 	public AddEventPage(AFStudiumAPIClientService apiClient, Subject subject)
 	{
 		InitializeComponent();
@@ -27,9 +28,10 @@ public partial class AddEventPage : ContentPage
         InitializeComponent();
         _apiClient = apiClient;
 		Event SelectedEvent = e;
+		subject = new Subject();
+
+		//var tasksubject = _apiClient.GetSubjects();//.Result.Where(n=> n.SubjectId == SelectedEvent.SubjectId).FirstOrDefault();(Task<Subject>)s
 		
-		
-		//Task<Subject> tasksubject =  (Task<Subject>)s;//_apiClient.GetSubjects().Result.Where(n=> n.SubjectId == SelectedEvent.SubjectId).FirstOrDefault();
 		
         thisContext = new ModulesViewModel(_apiClient);
 		thisContext.EventId = SelectedEvent.EventId;
@@ -37,22 +39,24 @@ public partial class AddEventPage : ContentPage
 		thisContext.EventName = SelectedEvent.EventName;//GetSubject(SelectedEvent).Result;
         thisContext.EventType = SelectedEvent.EventType;
         BindingContext = thisContext;
-		EventNameEntry.Text = e.EventName;
+        GetSubject(e);
+        EventNameEntry.Text = e.EventName;
 		AddBtn.Command = thisContext.EditEvent;
     }
 
-	public async Task<string> GetSubject(Event e)
+	public async void GetSubject(Event e)
 	{
         //return await _apiClient.GetSubjectById(e.SubjectId);
-        var tasksubject = await _apiClient.GetSubjectById(e.SubjectId);
+        var tasksubject = await _apiClient.GetSubjects();
        if (tasksubject != null)
        {
-            return tasksubject.SubjectName;
+            foreach (Subject sub in tasksubject)
+            {
+				if (sub.SubjectId == e.SubjectId)
+					thisContext.SubjectName = sub.SubjectName;
+            }
        }
-	   else
-	{
-			return "";
-	}
+	   
     }
 
 	public void ChangeText(object sender, EventArgs e)
@@ -62,10 +66,21 @@ public partial class AddEventPage : ContentPage
 		{
 			EventNameEntry.Text += $" {EventPicker.SelectedItem.ToString()}";
 		}
-		else if (eventname.Length == 2)
+		else if (eventname.Length > 1)
 		{
-			EventNameEntry.Text = eventname[0] + $" {EventPicker.SelectedItem.ToString()}";
+
+			//if (EventPicker.SelectedItem != null)
+			//{
+				EventNameEntry.Text = "";
+			//          for (int i = 0; i < eventname.Length-1; i++)
+			//          {
+			//              EventNameEntry.Text += eventname[i];
+			//          }
+			EventNameEntry.Text += thisContext.SubjectName; 
+            EventNameEntry.Text += $" {EventPicker.SelectedItem.ToString()}";
+        }
+			
 
         }
 	}
-}
+
